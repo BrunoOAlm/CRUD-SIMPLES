d19f88ae18ad052b6fa28d1e85904aa4c2938dfc
Bruno <brunoalm75@gmail.com>
2026-04-25 18:43:34 -0300

    implement JWT authentication, security config and CORS

--- src/main/java/com/bruno/CRUD/business/AuthService.java
+++ src/main/java/com/bruno/CRUD/business/AuthService.java
@@ -5,38 +5,47 @@ import com.bruno.CRUD.business.dtos.in.RegisterRequest;
 import com.bruno.CRUD.business.dtos.out.LoginResponse;
 import com.bruno.CRUD.infrastructure.entity.Usuario;
 import com.bruno.CRUD.infrastructure.repository.UsuarioRepository;
+import com.bruno.CRUD.infrastructure.security.JwtService;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class AuthService {
     private final UsuarioRepository usuarioRepository;
+    private final BCryptPasswordEncoder encoder;
+    private final JwtService jwtService;
 
-    public AuthService(UsuarioRepository usuarioRepository) {
+    public AuthService(UsuarioRepository usuarioRepository, JwtService jwtService) {
         this.usuarioRepository = usuarioRepository;
+        this.jwtService = jwtService;
+        this.encoder = new BCryptPasswordEncoder();
     }
+
 
     public LoginResponse login(LoginRequest request) {
 
         String email = request.getEmail();
         String senha = request.getSenha();
 
+
         if (email == null || senha == null) {
             throw new RuntimeException("Dados inválidos");
         }
 
-        Usuario usuario = usuarioRepository.findByEmail(request.getEmail())
+        Usuario usuario = usuarioRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
-        if (!usuario.getSenha().equals(request.getSenha())) {
+        if (!encoder.matches(senha, usuario.getSenha())) {
             throw new RuntimeException("Senha inválida");
         }
 
         // buscar no baco depois
 
-        return new LoginResponse("token-fake");
+        String token = jwtService.generateToken(usuario.getEmail());
+        return new LoginResponse(token);
     }
 
-    public void registrar(RegisterRequest request) {
+    public void register(RegisterRequest request) {
         String email = request.getEmail();
         String senha = request.getSenha();
 
@@ -44,9 +53,13 @@ public class AuthService {
             throw new RuntimeException("Dados inválidos");
         }
 
+        if (usuarioRepository.findByEmail(email).isPresent()) {
+            throw new RuntimeException("Usuário já existe");
+        }
+
         Usuario usuario = new Usuario();
-        usuario.setEmail(request.getEmail());
-        usuario.setSenha(request.getSenha());
+        usuario.setEmail(email);
+        usuario.setSenha(encoder.encode(senha));
 
         usuarioRepository.save(usuario);
 
---
+++ src/main/java/com/bruno/CRUD/infrastructure/security/JwtFilter.java
@@ -0,0 +1,44 @@
+package com.bruno.CRUD.infrastructure.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        // 🔥 verifica se tem Bearer token
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            String token = authHeader.substring(7);
+
+            try {
+                jwtService.validateToken(token);
+                String email = jwtService.getEmailFromToken(token);
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
