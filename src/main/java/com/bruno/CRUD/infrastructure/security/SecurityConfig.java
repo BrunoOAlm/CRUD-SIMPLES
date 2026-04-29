c903c6abe569dc59cf6ea21c319b7bcff2cc441c
Bruno <brunoalm75@gmail.com>
2026-04-29 09:01:32 -0300

    confirgurações finais do cors

--- src/main/java/com/bruno/CRUD/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/bruno/CRUD/infrastructure/security/SecurityConfig.java
@@ -50,7 +50,12 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000",
+                "https://crudhub.vercel.app",
+                "https://*.vercel.app"
+
+
+                ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
